e09f429fe6d38ad77f1842d0f6a603eb249cfda5
Dan Rod <supmercode@gmail.com>
2024-09-01 13:52:31 -0600

    First commit

---
+++ src/main/java/dev/dan/testing_apis/TestingApisApplication.java
@@ -0,0 +1,13 @@
+package dev.dan.testing_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dan/testing_apis/TestingApisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dan.testing_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
